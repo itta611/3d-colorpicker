9abaf8a4683cae3bbc98964999d4c29f46c1b75f
Itta Funahashi <itta@itta.dev>
2022-07-06 07:29:18 +0900

    fix camera position

--- components/ColorCubes.jsx
+++ components/ColorCubes.jsx
@@ -23,8 +23,9 @@ function ColorCubes() {
 
     const scene = new THREE.Scene();
 
-    const camera = new THREE.OrthographicCamera(-scW / 2, scW / 2, scH / 2, -scH / 2, 1, 1000);
-    camera.position.copy(new THREE.Vector3(-0.5, 1.2, 0));
+    const camera = new THREE.OrthographicCamera(scW / -2, scW / 2, scH / 2, scH / -2, 1, 1000);
+    camera.position.set(-400, 200, 400);
+
     camera.lookAt(target);
 
     const controls = new OrbitControls(camera, renderer.domElement);
@@ -37,12 +38,11 @@ function ColorCubes() {
         cubes[x].push([]);
         for (let z = 0; z < 6; z++) {
           const cube = new THREE.Mesh(
-            new THREE.BoxGeometry(10, 10, 10),
+            new THREE.BoxBufferGeometry(10, 10, 10),
             new THREE.MeshBasicMaterial({
               color: (0xff / 5) * x * 0x10000 + (0xff / 5) * y * 0x100 + (0xff / 5) * z,
             })
           );
-          console.log((0xff / 6) * x);
           cube.position.x = x * 15 - 30;
           cube.position.y = y * 15 - 30;
           cube.position.z = z * 15 - 30;
