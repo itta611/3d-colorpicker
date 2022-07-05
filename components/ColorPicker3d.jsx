e3ae465b44173d53ba72e0770119c901c6a376b2
Itta Funahashi <itta@itta.dev>
2022-07-05 16:21:33 +0900

    create state

--- components/ColorPicker3d.jsx
+++ components/ColorPicker3d.jsx
@@ -6,13 +6,27 @@ import {
   PopoverBody,
   Button,
   Box,
+  Input,
 } from '@chakra-ui/react';
 import ColorBox from './ColorBox';
-import { useState, useRef } from 'react';
+import { useState, useRef, createRef, useEffect } from 'react';
+
+function validateColor(hex) {
+  // just check it is a hex color
+  const regex = /^#[0-9a-f]*$/i;
+  return regex.test(hex);
+}
 
 function ColorPicker3d() {
-  const [currentColor, setCurrentColor] = useState('teal.400');
+  const [currentColor, setCurrentColor] = useState('#38B2AC');
   const ref = useRef();
+  const handleChange = (e) => {
+    let hex = e.target.value;
+    if (hex[0] === '#') {
+      setCurrentColor(hex);
+    }
+  };
+
   return (
     <Popover>
       <PopoverTrigger>
@@ -22,6 +36,7 @@ function ColorPicker3d() {
         <PopoverArrow />
         <PopoverBody>
           <Box ref={ref} h="xs"></Box>
+          <Input value={currentColor} maxLength={7} onChange={handleChange} />
         </PopoverBody>
       </PopoverContent>
     </Popover>
