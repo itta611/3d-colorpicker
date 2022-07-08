e53fd87fc2136cb6b3e01fb1d4f4aa397612c257
Itta Funahashi <itta@itta.dev>
2022-07-09 08:26:35 +0900

    dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import theme from './theme';
 
 function App({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
