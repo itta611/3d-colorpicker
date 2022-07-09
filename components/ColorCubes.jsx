d6d4150493d0d48cbbfd6669a0b938b3956c25af
Itta Funahashi <itta@itta.dev>
2022-07-09 15:26:24 +0900

    start render when popover completly open

--- components/ColorCubes.jsx
+++ components/ColorCubes.jsx
@@ -45,7 +45,67 @@ function ColorCubes({ saturation, setCurrentColor, popoverOpenRef }) {
 
   popoverOpenRef.current = () => {
     setTimeout(() => {
-      setRect(refContainer.current);
+      const { current: container } = refContainer;
+      setRect(container);
+      const target = new THREE.Vector3(0, 0, 0);
+
+      const renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        alpha: true,
+      });
+      renderer.setPixelRatio(window.devicePixelRatio);
+      renderer.setSize(scW, scH);
+      renderer.outputEncoding = THREE.sRGBEncoding;
+      container.appendChild(renderer.domElement);
+
+      const scene = new THREE.Scene();
+
+      camera = new THREE.OrthographicCamera(scW / -2, scW / 2, scH / 2, scH / -2, 1, 1000);
+      camera.position.set(400, 200, 400);
+
+      camera.lookAt(target);
+
+      const controls = new OrbitControls(camera, renderer.domElement);
+      controls.target = target;
+      controls.enablePan = false;
+      controls.enableZoom = false;
+      controls.enableDamping = true;
+      controls.dampingFactor = 0.1;
+
+      cubes = [];
+      for (let x = 0; x < size; x++) {
+        cubes.push([]);
+        for (let y = 0; y < size; y++) {
+          cubes[x].push([]);
+          for (let z = 0; z < size; z++) {
+            const cube = new THREE.Mesh(
+              new THREE.BoxBufferGeometry(15, 15, 15),
+              new THREE.MeshBasicMaterial({
+                transparent: true,
+                color:
+                  (0xff / (size - 1)) * x * 0x10000 +
+                  (0xff / (size - 1)) * y * 0x100 +
+                  (0xff / (size - 1)) * z,
+              })
+            );
+            const spacing = 20;
+            const centerDiff = (spacing * (size - 1)) / 2;
+            cube.position.x = x * spacing - centerDiff;
+            cube.position.y = y * spacing - centerDiff;
+            cube.position.z = z * spacing - centerDiff;
+            cubes[x][y].push(cube);
+            scene.add(cube);
+          }
+        }
+      }
+
+      const animate = () => {
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+        controls.update();
+        TWEEN.update();
+      };
+      animate();
     }, 250);
   };
 
@@ -87,73 +147,6 @@ function ColorCubes({ saturation, setCurrentColor, popoverOpenRef }) {
     const notDrag = e.clientX == mouseDownX && e.clientY == mouseDownY;
     if (hoveringColor && notDrag) setCurrentColor(hoveringColor);
   };
-
-  useEffect(() => {
-    const { current: container } = refContainer;
-    setRect(container);
-    const target = new THREE.Vector3(0, 0, 0);
-
-    const renderer = new THREE.WebGLRenderer({
-      antialias: true,
-      alpha: true,
-    });
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(scW, scH);
-    renderer.outputEncoding = THREE.sRGBEncoding;
-    container.appendChild(renderer.domElement);
-
-    const scene = new THREE.Scene();
-
-    camera = new THREE.OrthographicCamera(scW / -2, scW / 2, scH / 2, scH / -2, 1, 1000);
-    camera.position.set(400, 200, 400);
-
-    camera.lookAt(target);
-
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.target = target;
-    controls.enablePan = false;
-    controls.enableZoom = false;
-    controls.enableDamping = true;
-    controls.dampingFactor = 0.1;
-
-    cubes = [];
-    for (let x = 0; x < size; x++) {
-      cubes.push([]);
-      for (let y = 0; y < size; y++) {
-        cubes[x].push([]);
-        for (let z = 0; z < size; z++) {
-          const cube = new THREE.Mesh(
-            new THREE.BoxBufferGeometry(15, 15, 15),
-            new THREE.MeshBasicMaterial({
-              transparent: true,
-              color:
-                (0xff / (size - 1)) * x * 0x10000 +
-                (0xff / (size - 1)) * y * 0x100 +
-                (0xff / (size - 1)) * z,
-            })
-          );
-          const spacing = 20;
-          const centerDiff = (spacing * (size - 1)) / 2;
-          cube.position.x = x * spacing - centerDiff;
-          cube.position.y = y * spacing - centerDiff;
-          cube.position.z = z * spacing - centerDiff;
-          cubes[x][y].push(cube);
-          scene.add(cube);
-        }
-      }
-    }
-
-    let req = null;
-
-    const animate = () => {
-      req = requestAnimationFrame(animate);
-      renderer.render(scene, camera);
-      controls.update();
-      TWEEN.update();
-    };
-    animate();
-    return () => cancelAnimationFrame(req);
-  }, []);
 
   useEffect(() => {
     activeCubes = [];
